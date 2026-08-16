67b926642296b5071ec4bc756ce43e32d4b1764e
wiktort <palajestpotobystala123456788@gmaicom>
2026-08-16 20:19:02 +0200

    jh

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key = "supertajne"
+app.secret_key = os.environ.get("SECRET_KEY", "supertajne")
 
 
 def polacz_z_baza():
